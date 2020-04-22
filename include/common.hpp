6c6ba72164faf77cf9e7afe392988b8d408242af
Debbieliang9 <debbieliang-123@berkeley.edu>
2020-04-22 22:38:03 +0000

    k=1

--- include/common.hpp
+++ include/common.hpp
@@ -34,7 +34,7 @@ const string kMetadataIdentifier = "ANNA_METADATA";
 const string kMetadataDelimiter = "|";
 const char kMetadataDelimiterChar = '|';
 const string kMetadataTypeCacheIP = "cache_ip";
-const unsigned kNumShortestPaths = 4;
+const unsigned kNumShortestPaths = 1;
 
 inline void split(const string& s, char delim, vector<string>& elems) {
   std::stringstream ss(s);
